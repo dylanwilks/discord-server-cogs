5c3042b48bafda1854125713b6914c4809291113
dylanwilks <dylanwilks@github.com>
2025-12-01 21:31:39 +0000

    Updated Config class to use UserDict. Added type hints.

--- lib/config.py
+++ lib/config.py
@@ -1,32 +1,32 @@
 import json
 import pathlib 
-from typing import Self, Dict 
+from collections import UserDict
+from typing import Dict, Union, Any
 
 
-class Config(dict):
-    __setattr__ = dict.__setitem__
-    __delattr__ = dict.__delitem__
-
-    def __init__(self, *args, **kwargs) -> None:
+class Config(UserDict):
+    def __init__(self, *args: Dict[str, Any], **kwargs: Any) -> None:
         super().__init__(*args, **kwargs)
 
-    def __getitem__(self, key):
-        if (type(self.get(key)) is dict):
-            return Config(self.get(key))
-        else:
-            return self.get(attr)
+    def __getitem__(self, key: str) -> Union[Dict[str, Any], Any]:
+        val = self.data[key]
+        if (type(val) is dict):
+            self.data[key] = Config(val)
+            
+        return self.data[key]
 
-    def __getattr__(self, attr):
-        if (type(self.get(attr)) is dict):
-            return Config(self.get(attr))
-        else:
-            return self.get(attr)
+    def __getattr__(self, attr: str) -> Union[Dict[str, Any], Any]:
+        val = self.get(attr)
+        if (type(val) is dict):
+            self.data[attr] = Config(val)
+            
+        return self.data[attr]
 
-    def copy(self) -> Self:
+    def copy(self) -> Config:
         return Config(dict(self))
 
     @classmethod
-    def from_json(cls, config_file: str) -> Self:
+    def from_json(cls, config_file: str) -> Config:
         try:
             with open(pathlib.Path(config_file), 'r') as file:
                 config = json.load(file)
