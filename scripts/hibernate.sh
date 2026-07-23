a14f5371a74a446878448e5c81024af04eeb4b5f
w92704dw <dylan.wilks@student.manchester.ac.uk>
2026-07-23 01:42:27 +0100

    Use server name rather than IP address

--- scripts/hibernate.sh
+++ scripts/hibernate.sh
@@ -2,8 +2,11 @@
 
 ARP_LINE="arp -a | grep '$1'"
 ARP_LINE=$(eval "$ARP_LINE")
-IP=$(echo $ARP_LINE | cut -d " " -f 2 | tr -d '()')
+if [ -z "$ARP_LINE" ]; then
+	echo "Specified device not found."
+	exit 1
+fi
 
-scp /usr/src/app/webhooks/$1/* $2@$IP:$3
-scp /usr/src/app/scripts/message.sh $2@$IP:$4
-ssh -f $2@$IP "nohup doas zzz -Z > /dev/null 2>&1 &"
+scp /usr/src/app/webhooks/$1/* $2@$1:$3
+scp /usr/src/app/scripts/message.sh $2@$1:$4
+ssh -f $2@$1 "nohup doas zzz -Z > /dev/null 2>&1 &"
